7ba0094621714b8682b57feacb8150eb9b849e90
Name Inane <nameinane@gmail.com>
2013-07-22 16:44:50 -0400

    add some routes simplifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Syn::Application.routes.draw do
-  get "static_pages/home"
-
+  root 'static_pages#home'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,25 +2,39 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:link_text) { "Ruby on Rails Tutorialzz" }
+  let(:link_text) { "Yizkor" }
 
   describe "Home page" do
 
-    it "should have the content 'Holmesz'" do
-      visit '/static_pages/home'
-      expect(page).to have_title('Holmesz')
+    it "should have title 'Main page'" do
+      visit root_path
+      expect(page).to have_title('Main Page')
     end
 
-    it "should have the content 'Find me'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('me')
+    it "should have the content 'Yizkor'" do
+      visit root_path
+      expect(page).to have_content('Yizkor')
     end
 
-    it "should have a link with content 'Ruby on Rails Tutorial'" do
-      visit '/static_pages/home'
+    it "should have a link with content 'Yizkor'" do
+      visit root_path
       expect(page).to have_content("#{link_text}")
     end
+  
+  end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title('Contact')
+    end
 
   end
+
 end
